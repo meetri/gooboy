9772e8cc8b283a548e677733dc4163ba5cb05cfb
demetrius bell <meetri@gmail.com>
2019-01-16 21:13:36 -0800

    create multiple levels and menu screen

---
+++ levels/level1/level.go
@@ -0,0 +1,43 @@
+components {
+  id: "map"
+  component: "/levels/level1/map.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"/levels/level1/map.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"land\"\n"
+  "mask: \"character\"\n"
+  "mask: \"enemy\"\n"
+  "mask: \"bonus\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
